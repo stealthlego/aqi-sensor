304c2cb94ff1be3538a7eead6bd5f01b608b77f1
stealthlego <walexanderanderson@gmail.com>
2021-08-13 15:04:08 -0700

    Updated to add logging and working dockerfile

--- sensor.py
+++ sensor.py
@@ -1,9 +1,12 @@
 import requests
+import logging
 import os
 from dotenv import load_dotenv
 import paho.mqtt.client as mqtt 
 import paho.mqtt.publish as publish
 import time
+
+logging.basicConfig(level=logging.INFO)
 
 load_dotenv()
 
@@ -11,7 +14,7 @@ api_key = os.getenv('API_KEY')
 pw = os.getenv('PASSWORD')
 
 mqttBroker ="192.168.1.199" 
-api_string = f'https://www.airnowapi.org/aq/forecast/zipCode/?format=application/json&zipCode=98103&date=2021-08-01&distance=2&API_KEY={api_key}'
+api_string = f'https://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=98103&distance=5&API_KEY=A4578CF7-E787-4494-8C42-6DE04D1B473B'
 
 publish.single('home-assistant/aqi/number', str(0), hostname=mqttBroker)
 
@@ -22,8 +25,11 @@ client.loop_start()
 
 while True:
     response = requests.get(url=api_string)
-    aqi = response.json()[0]['AQI']
-    info = client.publish("home-assistant/aqi/number", aqi)
-    print(info.is_published())
-    print(f'Just published {aqi} to topic AQI')
-    time.sleep(10)
+    if response.status_code == 200:
+        aqi = response.json()[1]['AQI']
+        info = client.publish("home-assistant/aqi/number", aqi)
+        # logging.info(info.is_published())
+        logging.info(f'Just published {aqi} to topic AQI')
+        time.sleep(120)
+    else:
+        logging.warning('Error with API')
